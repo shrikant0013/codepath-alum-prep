201fdc6b5a11af54a8602b34136509087ce77107
Shrikant Pandhare <shrikant0013@gmail.com>
2017-07-09 23:35:56 -0700

    Create NextGreater.java

---
+++ NextGreater.java
@@ -0,0 +1,20 @@
+public class NextGreater {
+    public ArrayList<Integer> nextGreater(List<Integer> a) {
+        ArrayList<Integer> result =  new ArrayList<>();
+
+        for (int i = 0 ; i < a.size(); i++) {
+            boolean found = false;
+            for (int j = i; j < a.size(); j++) {
+                if (a.get(j) > a.get(i)) {
+                    found = true;
+                    result.add(i, a.get(j));
+                    break;
+                }
+            }
+            if (!found) {
+                result.add(i, -1);
+            }
+        }
+        return result;
+    }
+}
