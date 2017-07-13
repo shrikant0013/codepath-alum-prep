6fef982c2aaa06df9748f5e4df072292bdce4386
Shrikant Pandhare <shrikant0013@gmail.com>
2017-07-13 01:17:13 -0700

    Create LargestNumber.java

---
+++ LargestNumber.java
@@ -0,0 +1,27 @@
+public class LargestNumber {
+	// DO NOT MODIFY THE LIST
+	public String largestNumber(final List<Integer> a) {
+	    List<Integer> copy = new ArrayList<>(a);
+
+        Collections.sort(copy, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                String s1 = o1 + "" + o2;
+                String s2 = o2 + "" + o1;
+                return s2.compareTo(s1);
+            }
+        });
+
+        StringBuilder result = new StringBuilder();
+        for (int i : copy) {
+            result.append(i);
+        }
+        Integer resultInt = null;
+        try {
+            resultInt = Integer.parseInt(result.toString());
+        } catch (NumberFormatException e) {
+            return result.toString();
+        }
+        return resultInt.toString();
+	}
+}
